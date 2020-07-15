0ca07b677568cca443da61be372b76be77fd1b56
Viktor <viktor.s@almusmedia.com>
2020-07-15 14:30:32 +0300

    snap pixel

---
+++ snap.js
@@ -0,0 +1,11 @@
+<!-- Snap Pixel Code -->
+(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+    a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+    r.src=n;var u=t.getElementsByTagName(s)[0];
+    u.parentNode.insertBefore(r,u);})(window,document,
+    'https://sc-static.net/scevent.min.js');
+    snaptr('init', '9756c43c-f1e1-4350-9b04-82a470978f57', {
+'user_email': ''
+});
+snaptr('track', 'PAGE_VIEW');
